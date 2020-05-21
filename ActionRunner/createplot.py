3b0f7fba421ede2d66213e629e41bd59490bcdfe
Eri Airlangga <eri.airlangga@gmail.com>
2020-05-21 08:58:16 +0700

    Update createplot.py

--- ActionRunner/createplot.py
+++ ActionRunner/createplot.py
@@ -39,7 +39,7 @@ axs[1].xaxis_date()
 axs[0].xaxis_date()
 # simulating stacked bar
 axs[0].plot(x, Confirmed, color=c_Confirmed, label="Total Confirmed", linewidth=3)
-axs[0].bar(x, Hosp + Cured + Deaths, color = c_Hosp, label="Hospitalised")
+axs[0].bar(x, Hosp + Cured + Deaths, color = c_Hosp, label="Active")
 axs[0].bar(x, Cured + Deaths, color=c_Cured, label="Recovered")
 axs[0].bar(x, Deaths, color=c_Deaths, label="Deaths")
 axs[0].set_title("Indonesia COVID-19 Cumulative Confirmed Cases")
@@ -68,27 +68,37 @@ last_updated = dd.iloc[0,0]
 latest_confirmed = dd.iloc[0,6]
 latest_cured = dd.iloc[0,7]
 latest_deaths = dd.iloc[0,9] 
+latest_hosp = dd.iloc[0,8]
 
 latest_recov = dd.iloc[0,10] 
 latest_fatal = dd.iloc[0,11] 
 
 DI_latest_confirmed = dd.iloc[0,2] 
+DI_latest_hosp = dd.iloc[0,4] 
 DI_latest_cured = dd.iloc[0,3] 
 DI_latest_deaths = dd.iloc[0,5] 
 
 plt.gcf().text(0.78, 0.918, last_updated, color="#888888", fontsize=18, family="Arial", weight="bold", bbox=dict(facecolor="none", edgecolor="none",  boxstyle='square,pad=.7'))
 plt.gcf().text(0.78, 0.955, "Last Updated", color="#888888", fontsize=10,  family="Arial", weight="bold")
-plt.gcf().text(0.22, 0.918, latest_confirmed, color="#ffffff", fontsize=23, family="Arial", weight="bold", bbox=dict(facecolor=c_Confirmed, edgecolor=c_Confirmed,  boxstyle='square, pad=.7'))
-plt.gcf().text(0.42, 0.918, latest_cured, color="#ffffff", fontsize=23,  family="Arial", weight="bold", bbox=dict(facecolor=c_Cured, edgecolor=c_Cured,  boxstyle='square,pad=.7'))
+
+plt.gcf().text(0.07, 0.918, latest_confirmed, color="#ffffff", fontsize=23, family="Arial", weight="bold", bbox=dict(facecolor=c_Confirmed, edgecolor=c_Confirmed,  boxstyle='square, pad=.7'))
+plt.gcf().text(0.07, 0.955, "Confirmed", color="#ffffff", fontsize=10,  family="Arial", weight="bold")
+plt.gcf().text(0.07, 0.9, '+' + str(DI_latest_confirmed), color="#ffffff", fontsize=10,  family="Arial", weight="bold")
+
+plt.gcf().text(0.44, 0.918, latest_cured, color="#ffffff", fontsize=23,  family="Arial", weight="bold", bbox=dict(facecolor=c_Cured, edgecolor=c_Cured,  boxstyle='square,pad=.7'))
+plt.gcf().text(0.44, 0.955, "Recovered", color="#ffffff", fontsize=10,  family="Arial", weight="bold")
+plt.gcf().text(0.44, 0.9, '+' + str(DI_latest_cured), color="#ffffff", fontsize=10,  family="Arial", weight="bold")
+plt.gcf().text(0.49, 0.9, f"{latest_recov/100:.1%}", color="#ffffff", fontsize=10,  family="Arial", weight="bold")
+
 plt.gcf().text(0.62, 0.918, latest_deaths, color="#ffffff", fontsize=23,  family="Arial", weight="bold", bbox=dict(facecolor=c_Deaths, edgecolor=c_Deaths,  boxstyle='square,pad=.7'))
-plt.gcf().text(0.22, 0.955, "Confirmed", color="#ffffff", fontsize=10,  family="Arial", weight="bold")
-plt.gcf().text(0.42, 0.955, "Cured", color="#ffffff", fontsize=10,  family="Arial", weight="bold")
-plt.gcf().text(0.62, 0.955, "Deaths", color="#ffffff", fontsize=10,  family="Arial", weight="bold")
-plt.gcf().text(0.22, 0.9, '+' + str(DI_latest_confirmed), color="#ffffff", fontsize=10,  family="Arial", weight="bold")
-plt.gcf().text(0.42, 0.9, '+' + str(DI_latest_cured), color="#ffffff", fontsize=10,  family="Arial", weight="bold")
 plt.gcf().text(0.62, 0.9, '+' +str(DI_latest_deaths), color="#ffffff", fontsize=10,  family="Arial", weight="bold")
-plt.gcf().text(0.48, 0.9, f"{latest_fatal/100:.1%}", color="#ffffff", fontsize=10,  family="Arial", weight="bold")
-plt.gcf().text(0.68, 0.9, f"{latest_recov/100:.1%}", color="#ffffff", fontsize=10,  family="Arial", weight="bold")
+plt.gcf().text(0.62, 0.955, "Deaths", color="#ffffff", fontsize=10,  family="Arial", weight="bold")
+plt.gcf().text(0.68, 0.9, f"{latest_fatal/100:.1%}", color="#ffffff", fontsize=10,  family="Arial", weight="bold")
+
+plt.gcf().text(0.25, 0.918, latest_hosp, color="#ffffff", fontsize=23, family="Arial", weight="bold", bbox=dict(facecolor=c_Hosp, edgecolor=c_Hosp,  boxstyle='square, pad=.7'))
+plt.gcf().text(0.25, 0.955, "Active", color="#ffffff", fontsize=10,  family="Arial", weight="bold")
+plt.gcf().text(0.25, 0.9, '+' + str(DI_latest_hosp), color="#ffffff", fontsize=10,  family="Arial", weight="bold")
+
 
 fig.subplots_adjust(hspace=0.44, top=0.84, left=0.07, bottom=0.08, right=0.97)
 
