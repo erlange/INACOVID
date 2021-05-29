d3e66e7c12cbd8739f654902eb52513b72fc9fd2
erlange <eri.airlangga@gmail.com>
2021-05-29 14:18:08 +0700

    Clean web dist

--- web/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  baseHref: '/INACOVID/',
-  urlCase: 'https://raw.githubusercontent.com/erlange/INACOVID/master/data/json/all.min.json',
-  urlVax: 'https://raw.githubusercontent.com/erlange/INACOVID/master/data/json/vax.json'
-};
